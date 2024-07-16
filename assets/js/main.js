1eb2239a2dda038629818d25e20519278ffbb9ed
pracasweb <web@pracas.net>
2024-07-16 15:33:32 +0545

    update copyright year using js

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+  // Get the current year
+  var currentYear = new Date().getFullYear();
+
+  // Display it in the HTML element with id="current-year"
+  document.getElementById("current-year").textContent = currentYear;

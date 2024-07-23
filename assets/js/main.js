9b1fa53985a63600f822a53c77074ad5f630f3ca
Rimesh Bhattarai <77110341+rimeshinaruwa@users.noreply.github.com>
2024-07-23 09:50:46 +0545

    Initial Commit

--- assets/js/main.js
+++
@@ -1,5 +0,0 @@
-  // Get the current year
-  var currentYear = new Date().getFullYear();
-
-  // Display it in the HTML element with id="current-year"
-  document.getElementById("current-year").textContent = currentYear;

8d02180f485e0840ec4c30c78bdbb79454a0a7eb
Rimesh Bhattarai <77110341+rimeshinaruwa@users.noreply.github.com>
2024-07-30 15:52:54 +0545

    Updated
    Mobile-menu option for dropdown
    is written in JS code

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,13 @@
 
      // Display it in the HTML element with id="current-year"
      document.getElementById("current-year").textContent = currentYear;
+
+     // mobile-menu 
+     document.addEventListener("DOMContentLoaded", function() {
+          const hamburger = document.querySelector('.hamburger');
+          const menu = document.querySelector('#navbar-sticky');
+        
+          hamburger.addEventListener('click', function() {
+            menu.classList.toggle('hidden');
+          });
+        });
